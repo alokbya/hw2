b07901f913b2ae6e5f58b11dbde71032abc5269c
Alaaddin <alokby.alaaddin@gmail.com>
2018-05-29 15:14:15 -0700

    second

--- hw2.py
+++ hw2.py
@@ -3,15 +3,35 @@
 import datetime
 from webcam import *
 from PIL import ImageStat
+import matplotlib.pyplot as plt
 
+# stores intensities
+inte = []
+dates = []
+
+def plot_intense(arr1, arr2):
+    # get intensities
+    
+    # get times
+    plt.plot(arr1, arr2)
+    plt.show()
+    
+    return 0
 
 def call_back(image):
     # calculate the average intensity in the image
     intensity = ImageStat.Stat(image).mean[0]
     # show the image
-    image.show()
+    # image.show()
+    # store file 
+    # with open('intensities.csv', 'w') as f:
+    #     f.write(intensity)
+    #     f.write(datetime.datetime.now())
+    inte.append(intensity)
+    dates.append(datetime.datetime.now())
+    unit = []
+    
     print('Intensity: ' + str(intensity))
-    # print('You are a callback')
 
 def get_image():
     w = Webcam()
@@ -19,6 +39,8 @@ def get_image():
     w.start()
     input('Hit Enter to Stop \n')
     w.stop()
+    print(inte, dates)
+    plot_intense(dates, inte)
 
 if __name__ == '__main__':
     # create csv file
