a6376e32d97ac232e3ff414ed9a1ce48c11fa57b
Alaaddin <alokby.alaaddin@gmail.com>
2018-05-29 21:13:26 -0700

    broke up code into functions and recorded csv files with the data, made zip for joe

--- hw2.py
+++ hw2.py
@@ -4,43 +4,62 @@ import datetime
 from webcam import *
 from PIL import ImageStat
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
+class MUCamera:
+
+    def __init__(self):
 # stores intensities
-inte = []
-dates = []
+        self.inte = []
+        self.dates = []
+        self.nums = []
+        self.counter = 0
+        self.df = {}
+        
+        
 
-def plot_intense(arr1, arr2):
-    # get intensities
-    
-    # get times
-    plt.plot(arr1, arr2)
-    plt.show()
-    
-    return 0
+    def plot_intense(self, df):
+        # get times
 
-def call_back(image):
-    # calculate the average intensity in the image
-    intensity = ImageStat.Stat(image).mean[0]
-    # show the image
-    # image.show()
-    # store file 
-    # with open('intensities.csv', 'w') as f:
-    #     f.write(intensity)
-    #     f.write(datetime.datetime.now())
-    inte.append(intensity)
-    dates.append(datetime.datetime.now())
-    unit = []
-    
-    print('Intensity: ' + str(intensity))
+        plt.plot(df['Date'], df['Intensity'])
+        plt.ylabel('Intensity')
+        plt.xlabel('Time')
+        plt.show()
+        
+        return 0
 
-def get_image():
-    w = Webcam()
-    w.register_callback(call_back, 1)
-    w.start()
-    input('Hit Enter to Stop \n')
-    w.stop()
-    print(inte, dates)
-    plot_intense(dates, inte)
+    def to_csv(self, df):
+        df.to_csv('inte.csv', sep=',')
+            
+    def average_intensity(self, im):
+        return ImageStat.Stat(im).mean[0]
+
+    def filtered_average_intensity(self, im):
+        return 0
+
+    def call_back(self, image):
+        # calculate the average intensity in the image
+        intensity = self.average_intensity(image)
+        # show the image
+        # image.show()
+        self.inte.append(intensity)
+        self.dates.append(datetime.datetime.now().strftime("(%m-%d) %H:%M:%S"))
+        
+        self.df = pd.DataFrame({'Intensity': self.inte, 'Date': self.dates})
+        
+
+        print('Intensity: ' + str(intensity))
+
+    def get_image(self):
+        w = Webcam()
+        w.register_callback(self.call_back, 1)
+        w.start()
+        input('Hit Enter to Stop \n')
+        w.stop()
+        self.plot_intense(self.df)
+        self.to_csv(self.df)
+        print(self.df)
 
 if __name__ == '__main__':
     # create csv file
@@ -48,4 +67,5 @@ if __name__ == '__main__':
     #     f.write('Date/Time, Intensity')
         
     print(datetime.datetime.now())
-    get_image()
+    cam = MUCamera()
+    cam.get_image()
