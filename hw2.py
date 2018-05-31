7e6805eec35fab44b492e084544cec5eb200cf02
Alaaddin <alokby.alaaddin@gmail.com>
2018-05-30 19:47:29 -0700

    most recent update

--- hw2.py
+++ hw2.py
@@ -18,16 +18,61 @@ class MUCamera:
         self.df = {}
         
         
+    def mean_filter(self, l, width=3):
+        place_holder = 0
+        new = []
 
-    def plot_intense(self, df):
-        # get times
+        # Find the distance from center number to either side
+        side = int((width - 1)/2)
 
-        plt.plot(df['Date'], df['Intensity'])
+        # Iterate through list starting with left side of number
+        # To last number with side buffer
+        for i in range(side, len(l) - (side+1)+1):
+            for j in range(i-side, i+side+1):
+                place_holder = place_holder + l[j]
+            new.append(round(place_holder/width, 2))
+            place_holder = 0
+    
+        
+        return new
+    
+
+    def filtered_average_intensity(self):
+        df = pd.read_csv('inte.csv')
+        self.inte = df['Intensity'].tolist()
+        self.dates = df['Date'].tolist()
+        
+        
+        # clean up data (remove repeats)
+        # for i in range(len(inte) - 1):
+        #     if inte[i] == inte[i+1]:
+        #         del inte[i]
+        #         del dates[i]
+
+        # perform movmean averaging on data to smooth
+        # inte = mean_filter(inte, 3)
+        self.inte = mean_filter(self.inte, 150)
+        print(self.inte)
+        # print(dates)
+        index = list(range(len(self.inte)))
+        
+
+        
+        # plot the graph
+        plt.plot(index, self.inte)
         plt.ylabel('Intensity')
         plt.xlabel('Time')
         plt.show()
-        
-        return 0
+
+        def plot_intense(self, df):
+            # get times
+
+            plt.plot(df['Date'], df['Intensity'])
+            plt.ylabel('Intensity')
+            plt.xlabel('Time')
+            plt.show()
+            
+            return 0
 
     def to_csv(self, df):
         df.to_csv('inte.csv', sep=',')
@@ -66,6 +111,7 @@ if __name__ == '__main__':
     # with open('intensities.csv', 'w+') as f:
     #     f.write('Date/Time, Intensity')
         
-    print(datetime.datetime.now())
-    cam = MUCamera()
-    cam.get_image()
+    # print(datetime.datetime.now())
+    # cam = MUCamera()
+    # cam.get_image()
+    filtered_average_intensity()
---
+++ trial.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import pandas as pd
+import hw2
+import csv
+import matplotlib.pyplot as plt
+import math
+
+def mean_filter(l, width=3):
+    place_holder = 0
+    new = []
+
+    # Find the distance from center number to either side
+    side = int((width - 1)/2)
+
+    # Iterate through list starting with left side of number
+    # To last number with side buffer
+    for i in range(side, len(l) - (side+1)+1):
+        for j in range(i-side, i+side+1):
+            place_holder = place_holder + l[j]
+        new.append(round(place_holder/width, 2))
+        place_holder = 0
+    
+        
+    return new
+    
+
+def filtered_average_intensity():
+    df = pd.read_csv('inte.csv')
+    inte = df['Intensity'].tolist()
+    dates = df['Date'].tolist()
+    
+    
+    # clean up data (remove repeats)
+    # for i in range(len(inte) - 1):
+    #     if inte[i] == inte[i+1]:
+    #         del inte[i]
+    #         del dates[i]
+
+    # perform movmean averaging on data to smooth
+    # inte = mean_filter(inte, 3)
+    inte = mean_filter(inte, 150)
+    print(inte)
+    # print(dates)
+    index = list(range(len(inte)))
+    
+
+    
+    # plot the graph
+    plt.plot(index, inte)
+    plt.ylabel('Intensity')
+    plt.xlabel('Time')
+    plt.show()
+
+
+if __name__ == '__main__':
+    filtered_average_intensity()
